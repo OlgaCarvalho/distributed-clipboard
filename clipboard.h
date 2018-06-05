dbbb69509e58dbe008111d85ae9c99551918752f
OlgaCarvalho <olgasofiacarvalho@gmail.com>
2018-06-05 16:59:55 +0100

    testing doxygen comments

--- clipboard.h
+++ clipboard.h
@@ -15,7 +15,7 @@
 /** this function is called by the application to interact
  * with the distributed clipboard.
  * 
- * @param clipboard_dir - directory  where  the  local clipboard was launched.
+ * @param COISOETALLLLLLLLclipboard_dir - directory  where  the  local clipboard was launched.
  * 
  * @return -1 if cannot be accessed or >0 in case of success. 
  * It will be used in all other functions as clipboard_id.
